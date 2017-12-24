a539f1f747f19bd7595686b4b09c911bbdebf973
Anton Myshenin <myshenin.social@gmail.com>
2017-12-24 23:46:44 +0200

    done

---
+++ server/neumann-method/handler.js
@@ -0,0 +1,25 @@
+const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1);
+const binomial = (n, k, p) => factorial(n) / (factorial(k) * factorial(n - k)) * Math.pow(p, k) * Math.pow(1 - p, n - k);
+
+module.exports.neumannMethod = (event, context, callback) => {
+    const {n, p} = JSON.parse(event.body);
+    const c = binomial(n, Math.ceil(n / 2), p);
+    let point = {
+        x: null,
+        y: null
+    };
+
+    do {
+        point.x = Math.round(Math.random() * n);
+        point.y = Math.random() * c;
+    } while (point.y >= binomial(n, point.x, p));
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            value: point.x,
+        }),
+    };
+
+    callback(null, response);
+};
