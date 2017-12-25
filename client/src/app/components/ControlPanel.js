1dc32d24459efa64a41bba0f8c6183e6ff21e0f9
Anton Myshenin <myshenin.social@gmail.com>
2017-12-25 20:57:11 +0200

    sidebar v 1

--- client/src/app/components/ControlPanel.js
+++ client/src/app/components/ControlPanel.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {
+    LinearProgress, RaisedButton, Table, TableBody, TableHeader, TableHeaderColumn, TableRow,
+    TextField
+} from "material-ui";
+import {amber600, amber50, amber900} from "material-ui/styles/colors";
+
+const styles = {
+    underlineFocusStyle: {
+        borderColor: amber600,
+    },
+    floatingLabelStyle: {
+        color: amber50,
+        fontSize: '30px',
+        fontWeight: 'bold'
+    },
+    underlineStyle: {
+        borderColor: amber50,
+    },
+    inputStyle: {
+        color: amber900,
+        fontSize: '25px',
+        fontWeight: 'bold'
+    }
+};
 
 export class ControlPanel extends React.Component {
     constructor(props) {
@@ -9,7 +33,63 @@ export class ControlPanel extends React.Component {
     render() {
         return (
             <div className='control-panel'>
-                HELLO
+                <div className='control-panel-text n'>
+                    <TextField
+                        floatingLabelText="N"
+                        floatingLabelStyle={styles.floatingLabelStyle}
+                        underlineFocusStyle={styles.underlineFocusStyle}
+                        underlineStyle={styles.underlineStyle}
+                        inputStyle={styles.inputStyle}
+                    />
+                </div>
+                <div className='control-panel-text p'>
+                    <TextField
+                        floatingLabelText="p"
+                        floatingLabelStyle={styles.floatingLabelStyle}
+                        underlineFocusStyle={styles.underlineFocusStyle}
+                        underlineStyle={styles.underlineStyle}
+                        inputStyle={styles.inputStyle}
+                    />
+                </div>
+                <div className='control-panel-text iterations'>
+                    <TextField
+                        floatingLabelText="Итерации"
+                        floatingLabelStyle={styles.floatingLabelStyle}
+                        underlineFocusStyle={styles.underlineFocusStyle}
+                        underlineStyle={styles.underlineStyle}
+                        inputStyle={styles.inputStyle}
+                    />
+                </div>
+                <div className="start">
+                    <RaisedButton label="СТАРТ"/>
+                </div>
+                <div className="stop">
+                    <RaisedButton label="СТОП"/>
+                </div>
+                <div className="data">
+                    <Table>
+                        <TableHeader
+                            displaySelectAll={false}
+                            adjustForCheckbox={false}
+                        >
+                            <TableRow>
+                                <TableHeaderColumn>Величина</TableHeaderColumn>
+                                <TableHeaderColumn>Значение</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody
+                            displayRowCheckbox={false}
+                            adjustForCheckbox={false}
+                        ></TableBody>
+                    </Table>
+                </div>
+                <div className="progress">
+                    <LinearProgress
+                        mode="determinate"
+                        value={55}
+                        color={amber900}
+                    />
+                </div>
             </div>
         );
     }
