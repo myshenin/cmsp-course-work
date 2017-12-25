8436a71fdc1a6dd87609deda5397dc30b9016119
Anton Myshenin <myshenin.social@gmail.com>
2017-12-25 19:07:17 +0200

    Revert "general view"
    This reverts commit f18f0246c0a44b3096ffff19f933a7be94edd937.

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {ControlPanel} from "./conponents/ControlPanel";
-import {ResultsPanel} from "./conponents/ResultsPanel";
 
 class App extends React.Component {
     render() {
         return (
-           <div className='main'>
-               <ControlPanel/>
-               <ResultsPanel/>
-           </div>
+           <div>HELLO</div>
         );
     }
 }
