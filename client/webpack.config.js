e771b46c6d65ae55138db0d65cfd78f5c7e4748d
Anton Myshenin <myshenin.social@gmail.com>
2017-12-26 14:44:17 +0200

    webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const SRC_DIR = path.resolve(__dirname, "src");
+
+const config = {
+    entry: SRC_DIR + "\\app\\index.js",
+    output: {
+        path: DIST_DIR,
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+            }
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: './src/index.html',
+                to: './index.html'
+            },
+            {
+                from: './src/style.css',
+                to: './style.css'
+            }
+        ])
+    ]
+};
+
+module.exports = config;
