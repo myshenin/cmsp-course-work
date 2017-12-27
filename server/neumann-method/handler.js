9a63596b3097b3bd5e173da933ba95b0e0b024b0
Anton Myshenin <myshenin.social@gmail.com>
2017-12-27 20:39:13 +0200

    server fixes

--- server/neumann-method/handler.js
+++ server/neumann-method/handler.js
@@ -1,5 +1,5 @@
 const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1);
-const binomial = (n, k, p) => factorial(n) / (factorial(k) * factorial(n - k)) * Math.pow(p, k) * Math.pow(1 - p, n - k);
+const binomial = (n, k, p) => (factorial(n) / (factorial(k) * factorial(n - k))) * Math.pow(p, k) * Math.pow(1 - p, n - k);
 
 module.exports.neumannMethod = (event, context, callback) => {
     const {n, p} = event;
