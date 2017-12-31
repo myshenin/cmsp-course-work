44b2eef1e87d8435b4ab8499c5a0db7c3a4424d9
Anton Myshenin <myshenin.social@gmail.com>
2017-12-31 15:01:52 +0200

    values

--- client/src/app/components/DataBubble.js
+++ client/src/app/components/DataBubble.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {LinearProgress, Paper} from "material-ui";
-import {blueGrey500} from "material-ui/styles/colors";
+import {amber300, blueGrey500} from "material-ui/styles/colors";
+import {Carousel} from "react-responsive-carousel";
 
 const style = {
     backgroundColor: blueGrey500
@@ -11,11 +12,43 @@ export default class DataBubble extends React.Component {
         super(props);
     }
 
+    getSlides() {
+        return (<Carousel showThumbs={false} showStatus={false}>
+            <div className="value">
+                <div className="method">Аналитический метод</div>
+                <div className="m">M = {Math.round(this.props.next.next.values.analitic.M * 100) / 100}</div>
+                <div className="d">D = {Math.round(this.props.next.next.values.analitic.D * 100) / 100}</div>
+            </div>
+            <div className="value">
+                <div className="method">Метод обратных функций</div>
+                <div className="m">M = {Math.round(this.props.next.next.values.inverseTransformSampling.M * 100) / 100}</div>
+                <div className="d">D = {Math.round(this.props.next.next.values.inverseTransformSampling.D * 100) / 100}</div>
+            </div>
+            <div className="value">
+                <div className="method">Метод метрополиса</div>
+                <div className="m">M = {Math.round(this.props.next.next.values.metropolisMethod.M * 100) / 100}</div>
+                <div className="d">D = {Math.round(this.props.next.next.values.metropolisMethod.D * 100) / 100}</div>
+            </div>
+            <div className="value">
+                <div className="method">Метод Неймана</div>
+                <div className="m">M = {Math.round(this.props.next.next.values.neumannMethod.M * 100) / 100}</div>
+                <div className="d">D = {Math.round(this.props.next.next.values.neumannMethod.D * 100) / 100}</div>
+            </div>
+        </Carousel>);
+    }
+
     render() {
         return (
             <Paper className='data-bubble' circle={true} zDepth={2} style={style}>
                 <div>
-                    <LinearProgress mode="determinate" value={this.props.next.next.iteration} max={this.props.next.iterationAmount} />
+                    <div className="value-slider">
+                        {Object.keys(this.props.next.next).length !== 0 ? this.getSlides() : ''}
+                    </div>
+                    <LinearProgress mode="determinate"
+                                    value={this.props.next.next.iteration}
+                                    max={this.props.next.iterationAmount}
+                                    color={amber300}
+                    />
                 </div>
             </Paper>
         );
