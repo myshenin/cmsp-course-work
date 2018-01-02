f7e65d2197fa56a48f8a7bb730509f1159780dea
Anton Myshenin <myshenin.social@gmail.com>
2018-01-02 20:43:14 +0200

    buttons

--- client/src/app/containers/App.js
+++ client/src/app/containers/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import {MuiThemeProvider} from "material-ui";
+import {FloatingActionButton, MuiThemeProvider} from "material-ui";
 
 import ContentPaper from "../components/ContentPaper";
 import InputsBubble from "../components/InputsBubble";
@@ -15,8 +15,19 @@ import {connect} from "react-redux";
 
 import getNext from "../redux/actions/getNext";
 import setIterationAmount from "../redux/actions/setIterationAmount";
+import {FileFileDownload, FileFileUpload} from "material-ui/svg-icons/index";
+import {blueGrey500, brown500} from "material-ui/styles/colors";
+
 injectTapEventPlugin();
 
+const styles = {
+    download: {
+        backgroundColor: brown500
+    },
+    upload: {
+        backgroundColor: blueGrey500
+    }
+};
 
 class App extends React.Component {
     constructor(props) {
@@ -30,6 +41,16 @@ class App extends React.Component {
                     <InputsBubble next={this.props}/>
                     <ContentPaper next={this.props}/>
                     <DataBubble next={this.props}/>
+                    <div className="download">
+                        <FloatingActionButton backgroundColor={styles.download.backgroundColor}>
+                            <FileFileDownload/>
+                        </FloatingActionButton>
+                    </div>
+                    <div className="upload">
+                        <FloatingActionButton backgroundColor={styles.upload.backgroundColor}>
+                            <FileFileUpload/>
+                        </FloatingActionButton>
+                    </div>
                 </div>
             </MuiThemeProvider>
         );
